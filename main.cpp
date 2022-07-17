e726bbba654862f848bbd00c62266daed1bc5f94
Alexander <amaximuk@mail.ru>
2022-07-17 18:22:42 +0300

    Переименовал проект в cubes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 
 #include <QApplication>
+#include <QLabel>
 
 int main(int argc, char* argv[])
 {
 	QApplication app(argc, argv);
-
+    QLabel* lbl = new QLabel();
+    lbl->text() = "AAAAAAAAAA";
+    lbl->show();
 	return app.exec();
 };
