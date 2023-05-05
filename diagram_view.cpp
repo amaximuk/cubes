28ed1a4206b112518f8a7d9bec9f5ea3fa925be4
Alexander <amaximuk@mail.ru>
2023-05-05 20:57:36 +0300

    Удалил часть функций, связанных с группами

--- diagram_view.cpp
+++ diagram_view.cpp
@@ -90,9 +90,9 @@ void diagram_view::dropEvent(QDropEvent *event)
             diagram_scene* ds = qobject_cast<diagram_scene*>(this->scene());
 
             di->getProperties()->SetFileNames(main_->GetFileNames());
-            di->getProperties()->SetGroupNames(main_->GetGroupNames());
             di->getProperties()->SetFileName(main_->GetCurrentFileName());
-            di->getProperties()->SetGroupName(main_->GetCurrentGroupName());
+            di->getProperties()->SetGroupNames(main_->GetFileGroups(main_->GetCurrentFileName()));
+            di->getProperties()->SetGroupName("<not selected>");
             di->getProperties()->SetName(main_->GetNewUnitName(di->getProperties()->GetName(), main_->GetCurrentGroup()));
             di->SetGroupName(main_->GetCurrentGroup());
             ds->informItemCreated(di);
