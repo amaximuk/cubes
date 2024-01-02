74c3967b192542203d174a66f665c6f50513fadd
Alexander <amaximuk@mail.ru>
2024-01-02 18:12:18 +0300

    Переход на библиотеку parameters

--- 3rdparty/build.cmd
+++
@@ -1,9 +0,0 @@
-cd QtPropertyBrowser
-mkdir build
-cd build
-cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=../install -G "Visual Studio 16 2019" -A x64 ..
-cmake --build . --config Release
-cmake --install . --config Release
-cmake --build . --config Debug
-cmake --install . --config Debug
-cd ../..
